4fe6c9c95bba9ac3e2cbefe1c5649ba2fe79cb71
lasitha1991 <lasitha1991@hotmail.com>
2024-12-24 13:38:01 +0530

    run.sh added

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 cd ./frontend
 npm run build
-
 rm -rf ../backend/src/main/resources/static
 cp -r ./build/ ../backend/src/main/resources/static
+
+cd ../backend
+./mvnw clean install package
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -jar ./backend/target/payrollsystem-0.0.1-SNAPSHOT.jar
